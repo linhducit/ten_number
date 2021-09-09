d16652a74aa62ab22570183f1de6795dd0371baf
LinhDuc <linhduc.nguyen6394@gmail.com>
2021-09-09 22:52:23 +0700

    sfd

--- app/src/main/java/com/nld/contacts/Person.java
+++ app/src/main/java/com/nld/contacts/Person.java
@@ -4,8 +4,4 @@ public class Person {
     private Integer number;
     private Integer count;
     private String name;
-    private Integer age;
-    private Integer one;
-    private Integer two;
-    private Integer three;
 }
